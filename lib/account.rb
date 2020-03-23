c079f62c0db119273e2c842af753dd3a7c0c5924
Dafna Libman <dafnalibman@gmail.com>
2020-03-23 14:09:12 +0000

    Add a make deposit method

--- lib/account.rb
+++ lib/account.rb
@@ -1,5 +1,15 @@
 class Account
-  def balance
-    0
+
+  def initialize(balance=0)
+    @balance = balance
   end
+
+  def balance
+    @balance
+  end
+
+  def make_deposit(sum)
+    @balance = @balance + sum
+  end
+
 end
--- spec/balance_spec.rb
+++ spec/balance_spec.rb
@@ -2,9 +2,17 @@ require './lib/account.rb'
 
 describe 'Account' do
   describe 'balance' do
-    it 'shows a balance of zero' do
+    it 'shows an account balance of zero' do
       account = Account.new
       expect(account.balance).to eq 0
+    end
+  end
+
+  describe 'make deposite' do
+    it 'allows a client to add money to an account' do
+      account = Account.new 
+      account.make_deposit(10)
+      expect(account.balance).to eq 10
     end
   end
 end
